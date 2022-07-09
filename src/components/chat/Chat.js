927ea3644745bc9b2685c6f4833a732957887bec
awabaqibb <awabaqibb@gmail.com>
2022-07-09 23:28:46 +0500

    Final

--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -1,5 +1,8 @@
 import db from "../../firebase.config";
-import {useParams} from 'react-router-dom'
+import firebase from "firebase/app";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import useSound from "use-sound";
+import { useParams } from "react-router-dom";
 import "./Chat.css";
 import { Avatar } from "@mui/material";
 import { useState, useEffect } from "react";
@@ -10,40 +13,70 @@ import {
   InsertEmoticon,
   MicOutlined,
 } from "@mui/icons-material";
-import {
-  doc,
-  onSnapshot,
-} from "firebase/firestore";
+import { doc, onSnapshot, getDoc, addDoc, collection } from "firebase/firestore";
 
 import { IconButton } from "@mui/material";
 
 function Chat() {
+  const displayName = localStorage.getItem("displayName");
   const [seed, setSeed] = useState("");
   const [input, setInput] = useState("");
-  const {roomId} = useParams()
-  const [roomName, setRoomName] = useState("")
-  const [messages, setMessages] = useState("")
+  const { roomId } = useParams();
+  const [roomName, setRoomName] = useState("");
+  const [messagess, setMessages] = useState([]);
+  const [issendChecked, setIssendChecked] = useState(false);
+
+  const [playOn] = useSound(`${process.env.PUBLIC_URL}/send.mp3`, {
+    volume: 0.5,
+  });
+  const [playOff] = useSound(`${process.env.PUBLIC_URL}/send.mp3`, {
+    volume: 0.5,
+  });
+
+  const addNewUser=async()=> {
+    let auth = getAuth()
+    let user = auth.currentUser;
+
+    const docRef = await addDoc(collection(db, "rooms", roomId, "messages"), {
+      name: user.displayName,
+      message: input
+    });
+    console.log("Document written with ID: ", docRef.id);
+
+  }
 
   useEffect(() => {
     setSeed(Math.floor(Math.random() * 5000));
 
-    if(roomId){
-      console.log(roomId)
-      const colRef = doc(db, "rooms", roomId)
-      onSnapshot(colRef, (snapshot) => (
-        setRoomName(snapshot.data().name)
-      ))
+    if (roomId) {
+      const colRef = doc(db, "rooms", roomId);
+      onSnapshot(colRef, (snapshot) => setRoomName(snapshot.data().name));
+
+      const data = [];
+      let collectionRef = collection(db, "rooms", roomId, "messages");
+
+      onSnapshot(collectionRef, (querySnapshot) => {
+        querySnapshot.docs.forEach((doc) => {
+          data.push({ id: doc.id, data: doc.data() });
+        });
+        setMessages(data);
+      });
       
-      colRef.collection("messages").orderBy("timestamp", "asc").onSnapshot(
-        (snapshot)=> setMessages(snapshot.docs.map((doc) => doc.data()))
-      )
+      console.log(data);
     }
   }, [roomId]);
 
-  const sendMessage = (e) => {
-    e.preventDefault()
-    console.log(input)
-    setInput('')
+  const sendMessage = (e) => {    
+
+    e.preventDefault();
+    if (input.length > 0) {
+      
+      addNewUser()
+      
+      setIssendChecked(!issendChecked);
+      issendChecked ? playOff() : playOn();
+      setInput("");
+    }
   };
 
   return (
@@ -69,12 +102,13 @@ function Chat() {
         </div>
       </div>
 
+
       <div className="chat__body">
-        {messages.map((message)=>(
-          <p className={`chat__message ${true && "chat__receiver"}`}>
-          <span className="chat__name"> {message.name} </span>
-          {message.message}
-          <span className="chat__timestamp">{new Date(message.timestamp?.toDate()).toUTCString()}</span>
+        {messagess.map((message)=>(
+          <p key={message.id} className={`chat__message ${true && "chat__receiver"}`}>
+          <span className="chat__name"> {message.data.name} </span>
+          {message.data.message}
+          <span className="chat__timestamp">{message.timestamp}</span>
         </p>
         ))}
         
